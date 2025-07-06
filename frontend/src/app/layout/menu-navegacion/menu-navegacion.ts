d95a7764aa110789bb0e0c339fde24079c3250d7
adrisad <22082002andrademedina@gmail.com>
2025-07-06 14:57:48 -0400

    style(frontend):  estilo de menu de navegacion

--- frontend/src/app/layout/menu-navegacion/menu-navegacion.ts
+++ frontend/src/app/layout/menu-navegacion/menu-navegacion.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-menu-navegacion',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './menu-navegacion.html',
   styleUrl: './menu-navegacion.css'
 })
