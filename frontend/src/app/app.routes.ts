771a20d996011998f0899b13672fd8cfb2d49c34
adrisad <22082002andrademedina@gmail.com>
2025-07-06 14:58:25 -0400

    style(frontend): diseño del home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {MenuNavegacion} from './layout/menu-navegacion/menu-navegacion'
+import {Home} from './pages/home/home'
+export const routes: Routes = [
+  {
+    path: '',
+    component: MenuNavegacion,
+    children: [
+      { path: 'Inicio', component: Home },
+    ]
+  },
+];
