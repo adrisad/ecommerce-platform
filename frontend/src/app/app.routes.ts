6ccc9e550aa326270d30f71ea904d6e73a027374
adrisad <22082002andrademedina@gmail.com>
2025-07-07 12:52:02 -0400

    fix(frontend): correccion de rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,15 @@
-import { Routes } from '@angular/router';
-import {MenuNavegacion} from './layout/menu-navegacion/menu-navegacion'
+import { RouterModule, Routes } from '@angular/router';
 import {Home} from './pages/home/home'
+import {Product} from './pages/product/product'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 export const routes: Routes = [
-  {
-    path: '',
-    component: MenuNavegacion,
-    children: [
-      { path: 'Inicio', component: Home },
-    ]
-  },
+    { 
+        path: '', 
+        component: Home 
+    },
+    { 
+        path: 'productos', 
+        component: Product
+    },
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import {MenuNavegacion} from './layout/menu-navegacion/menu-navegacion'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule,MenuNavegacion],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
