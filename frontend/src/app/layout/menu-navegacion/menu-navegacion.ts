75deffcaf65ab44a103847a15b5298f503f968fd
adrisad <22082002andrademedina@gmail.com>
2025-07-07 12:50:24 -0400

    style(frontend): correccion de diseño del menu de navegacion

--- frontend/src/app/layout/menu-navegacion/menu-navegacion.ts
+++ frontend/src/app/layout/menu-navegacion/menu-navegacion.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink} from '@angular/router';
 @Component({
   selector: 'app-menu-navegacion',
-  imports: [RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './menu-navegacion.html',
   styleUrl: './menu-navegacion.css'
 })
-export class MenuNavegacion {
-
-}
+export class MenuNavegacion {}
